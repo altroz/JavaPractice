227e92911515e61207938de5a0f860dff0f45242
altroz <nhb913@outlook.com>
2015-06-06 15:33:11 -0700

    Illustrates the main difference between two Non-Access Modifier (Abstract + Final). Final Classes can instantiated but not subclassed. Abstract Class can subclassed and needs a concrete class to implement its methods. Abstract Classes cannot be directly instaniated.

---
+++ src/com/company/Delaration_AccessControl/AbstractClassTest.java
@@ -0,0 +1,8 @@
+package com.company.Delaration_AccessControl;
+
+public abstract class AbstractClassTest {
+	public static void main(String[]args){
+		System.out.println("The Abstract non-access class modifier can be subclassed but not instaniated. " );
+	}
+
+}
---
+++ src/com/company/Delaration_AccessControl/FinalClassTest.java
@@ -0,0 +1,12 @@
+package com.company.Delaration_AccessControl;
+
+public final class FinalClassTest {
+	
+	public void doSomething(){
+		System.out.println("Quick Test");
+	};
+	public static void main(String[]args){
+		System.out.println("This class examines a final Non-Access Modifier. Final class can be instantiated but not subclassed.");
+	}
+
+}
