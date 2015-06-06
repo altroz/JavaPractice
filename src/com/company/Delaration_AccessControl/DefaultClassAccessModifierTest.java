5d82d0ec7c1859360a129f47c1118be2c92c12e3
altroz <nhb913@outlook.com>
2015-06-06 14:47:47 -0700

    Initial Revision from Declaration & Access Control Chapter.

---
+++ src/com/company/Delaration_AccessControl/DefaultClassAccessModifierTest.java
@@ -0,0 +1,7 @@
+package com.company.Delaration_AccessControl;
+
+class DefaultClassAccessModifierTest {
+	public static void main(String[]args){
+		System.out.println("This is a default Class Access Modifier. Only the Class within the package can access it.");
+	};
+}
---
+++ src/com/company/Delaration_AccessControl/PublicAccessModifierTest.java
@@ -0,0 +1,6 @@
+package com.company.Delaration_AccessControl;
+
+public class PublicAccessModifierTest {
+	public static void main(String [] args){
+	System.out.println("This Class is a public class meaning that any class can access from outside package.");}
+}
