6ed257e8ffb50643399caa9b2b2815a8e8787081
altroz <nhb913@outlook.com>
2015-06-13 21:37:16 -0700

    The following code illustrates the looping way of populating the Ferrari object arrray.

--- src/com/company/ArrayTest/ArrayTest.java
+++ src/com/company/ArrayTest/ArrayTest.java
@@ -2,11 +2,11 @@ package com.company.ArrayTest;
 
 public class ArrayTest {
 	public static void main(String[]args){
-		//Declaring and initiating Array Object
-		int[] values = new int [5];
-		Ferrari [] ferrari = new Ferrari[5];
 		
-		//Constructing a Multidimensional Array. 
-		int [][]myArray =new myArray[3][];
+		Ferrari[] ferrari = new Ferrari[6];
+		for(int x=0; x<ferrari.lenght; x++){
+			ferrari[x] = new Ferrari();
+		}
+			
 	}
 }
