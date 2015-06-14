18b36ed527d6f906df9f389cbb72455a39a84e18
altroz <nhb913@outlook.com>
2015-06-14 14:24:21 -0700

    The usages and implementation of interface.

---
+++ src/com/company/TutorialTest/Living.java
@@ -0,0 +1,23 @@
+package com.company.TutorialTest;
+
+public interface Living {
+	
+	public void setName(String newName);
+	public String getName();
+
+	public void setHeight(double newHeight);
+	public double getHeight();
+	
+	public void setWeight(double newWeight);
+	public String getWeight();
+
+	public void setFavFood(String newFood);
+	public double getFavFood();
+	
+	public void setSpeed(double newSpeed);
+	public String getSpeed();
+
+	public void setSound(double newSound);
+	public double getSound();
+	
+}
