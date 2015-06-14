03b4931b6fcc8aa184357dfd98f5c6ae9732e6bf
altroz <nhb913@outlook.com>
2015-06-13 21:41:50 -0700

    The following code again, demostrates the various ways of constructing and initializing arrays.

--- src/com/company/ArrayTest/ArrayTest.java
+++ src/com/company/ArrayTest/ArrayTest.java
@@ -2,11 +2,13 @@ package com.company.ArrayTest;
 
 public class ArrayTest {
 	public static void main(String[]args){
+		Ferrari [] ferrari = {new Ferrari("One"), new Ferrari("Two"), new Ferrari("Three")};
+		int[] dots = {6,7,8};
 		
-		Ferrari[] ferrari = new Ferrari[6];
-		for(int x=0; x<ferrari.lenght; x++){
-			ferrari[x] = new Ferrari();
-		}
-			
+		int [][] races= {{3,6,7},{1,6},{5,6,5,6,5}};
+		
+		//Constructing and Initializing an Anonymous Array
+		int[] myArray;
+		myArray = new int []{3,6,2};
 	}
 }
