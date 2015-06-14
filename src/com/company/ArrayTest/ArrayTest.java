9be5072872db87dd3bfe8cf4b120a7b6b73d4425
altroz <nhb913@outlook.com>
2015-06-13 21:31:37 -0700

    The following codes are some of the ways of initiating and declaring variable arrays and object arrays.

---
+++ src/com/company/ArrayTest/ArrayTest.java
@@ -0,0 +1,12 @@
+package com.company.ArrayTest;
+
+public class ArrayTest {
+	public static void main(String[]args){
+		//Declaring and initiating Array Object
+		int[] values = new int [5];
+		Ferrari [] ferrari = new Ferrari[5];
+		
+		//Constructing a Multidimensional Array. 
+		int [][]myArray =new myArray[3][];
+	}
+}
