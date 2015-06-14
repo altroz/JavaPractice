c1674a35beedbd6581501662a57108f9db97dcfb
altroz <nhb913@outlook.com>
2015-06-14 11:31:38 -0700

    The following code example illustrates that - fields and methods are defined when you extend the super class. - One defines only the changes.

---
+++ src/com/company/TestInherit/Animal.java
@@ -0,0 +1,13 @@
+package com.company.TestInherit;
+
+//This Animal Class is a SuperClass
+
+public class Animal {
+	private String name;
+	private double height;
+	private int weight;
+	
+	public void setName(String newName){
+		name = newName;
+	}
+}
---
+++ src/com/company/TestInherit/Dog.java
@@ -0,0 +1,9 @@
+package com.company.TestInherit;
+
+// This Dog Class will inherit from the Animal Class
+
+public class Dog extends Animal{
+	public void digHole(){
+		System.out.println("Hole Dug");
+	}
+}
