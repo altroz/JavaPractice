b8e3828cbade5117789c9d9310fa8377f83a9bc2
altroz <nhb913@outlook.com>
2015-06-13 18:42:56 -0700

    The following regex code looks for the hexidecial code in the given source pattern.

--- src/com/company/RegexTest/RegexTest.java
+++ src/com/company/RegexTest/RegexTest.java
@@ -4,8 +4,8 @@ import java.util.regex.*;
 
 public class RegexTest {
 	public static void main(String[]args){
-		Pattern p = Pattern.compile("[a-cA-C]");
-		Matcher m = p.matcher("cafeBABE");
+		Pattern p = Pattern.compile("0[Xx][0-9a-fA-F]");
+		Matcher m = p.matcher("12 0x 0x12 0Xf 0xg");
 		
 		while(m.find()){
 			System.out.print(m.start()+" ");}
