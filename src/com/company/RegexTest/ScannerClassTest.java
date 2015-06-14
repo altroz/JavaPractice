ddb6985ad037bc0893209e66f1cedb1f4d48f23c
altroz <nhb913@outlook.com>
2015-06-13 20:07:21 -0700

    The usage of Token

---
+++ src/com/company/RegexTest/ScannerClassTest.java
@@ -0,0 +1,24 @@
+package com.company.RegexTest;
+import java.util.*;
+
+public class ScannerClassTest {
+	public static void main(String[]args){
+		System.out.print("input:	");
+		System.out.flush();
+		
+		try{
+			Scanner s= new Scanner(System.in);
+			String token;
+			
+			do{
+				token=s.findInLine(args[0]);
+				System.out.println("found "+token);}
+			while(token!=null);
+		
+		}
+		catch(Exception e){
+			System.out.println("scan exc");
+		}
+		
+	}
+}
