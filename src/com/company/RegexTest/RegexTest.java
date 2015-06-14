4c0bd141427c92c11f0ad171bcfb1d271d3d8e91
altroz <nhb913@outlook.com>
2015-06-13 19:47:48 -0700

    The usage of special quantifiers is being seen. The .*xx, refers to a greedy quantifier that looks for the occurence of "any character in front of xx" that occurs zero or more occurences. (However does not show any consecutive occurences of the same event)

--- src/com/company/RegexTest/RegexTest.java
+++ src/com/company/RegexTest/RegexTest.java
@@ -4,8 +4,8 @@ import java.util.regex.*;
 
 public class RegexTest {
 	public static void main(String[]args){
-		Pattern p = Pattern.compile("\\d\\d\\d([-\\s])?\\d\\d\\d\\d");
-		Matcher m = p.matcher("9876543,987 6543,987-6543");
+		Pattern p = Pattern.compile(".*xx");
+		Matcher m = p.matcher("yyxxxyxx");
 		
 		while(m.find()){
 			System.out.print(m.start()+" ");}
