7ac582e479d80bbba039e78346ffb97016bfec1a
altroz <nhb913@outlook.com>
2015-06-13 20:40:16 -0700

    There are several ways to initiate a String Object.

---
+++ src/com/company/StringTest/StringTest.java
@@ -0,0 +1,14 @@
+package com.company.StringTest;
+
+public class StringTest {
+	public static void main(String[]args){
+	//1
+		String a = new String();
+			   a="abc";
+		
+	//2
+		String b = new String("abc");
+	//3
+		String c = "abc";
+	}
+}
