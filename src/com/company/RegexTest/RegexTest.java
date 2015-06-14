3a2956f5518e4ae5948124acff9caeb66e7b728a
altroz <nhb913@outlook.com>
2015-06-13 18:46:41 -0700

    The following regex code looks for the multiple digits in the source text with the aid of the plus sign at the end of the \\d+

--- src/com/company/RegexTest/RegexTest.java
+++ src/com/company/RegexTest/RegexTest.java
@@ -4,8 +4,8 @@ import java.util.regex.*;
 
 public class RegexTest {
 	public static void main(String[]args){
-		Pattern p = Pattern.compile("0[Xx][0-9a-fA-F]");
-		Matcher m = p.matcher("12 0x 0x12 0Xf 0xg");
+		Pattern p = Pattern.compile("\\d+");
+		Matcher m = p.matcher("1 a44 555b");
 		
 		while(m.find()){
 			System.out.print(m.start()+" ");}
