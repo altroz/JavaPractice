49f1e2925481e0bfaf592ca7ebd495a0926b68f6
altroz <nhb913@outlook.com>
2015-06-15 19:09:28 -0700

    The usage of Interface and Monkey Class implements Living Interface.

---
+++ src/TestingPackage/Living.java
@@ -0,0 +1,21 @@
+package TestingPackage;
+
+public interface Living {
+	public void setName(String newName);
+	public String getName();
+	
+	public void setHeight(double newHeight);
+	public double getHeight();
+	
+	public void setWeight(double newWeight);
+	public String getWeight();
+	
+	public void setFavFood(String newFood);
+	public double getFavFood();
+	
+	public void setSpeed(double newSpeed);
+	public String getSpeed();
+	
+	public void setSound(double newSound);
+	public double getSound();
+}
---
+++ src/TestingPackage/Monkey.java
@@ -0,0 +1,79 @@
+package TestingPackage;
+
+public class Monkey implements Living{
+	private String name;
+	private double height;
+	private int weight;
+	@Override
+	public void setName(String newName) {
+		// TODO Auto-generated method stub
+		name = newName;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+
+	@Override
+	public void setHeight(double newHeight) {
+		// TODO Auto-generated method stub
+		height = newHeight;
+	}
+
+	@Override
+	public double getHeight() {
+		// TODO Auto-generated method stub
+		return height;
+	}
+
+	@Override
+	public void setWeight(double newWeight) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getWeight() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setFavFood(String newFood) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double getFavFood() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setSpeed(double newSpeed) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getSpeed() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setSound(double newSound) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double getSound() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
