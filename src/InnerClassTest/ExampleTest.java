fb55471efcfb8c1ad1e4807691224341a18ab7cf
altroz <nhb913@outlook.com>
2015-06-17 12:47:17 -0700

    The usage of a one-liner code to instanitate the inner Class

--- src/InnerClassTest/ExampleTest.java
+++ src/InnerClassTest/ExampleTest.java
@@ -2,8 +2,13 @@ package InnerClassTest;
 
 public class ExampleTest {
 	public static void main(String[] args){
-		ExampleOuterClass oc = new ExampleOuterClass();
+	/*	ExampleOuterClass oc = new ExampleOuterClass();
 		ExampleOuterClass.ExampleInnerClass ic =  oc. new ExampleInnerClass();
 		ic.accessOuter();
+		*/
+		
+	//One Liner Code
+		ExampleOuterClass.ExampleInnerClass inner = new ExampleOuterClass().ExampleInnerClass() ;
+		inner.accessOuter();
 	}
 }
