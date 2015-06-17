3fe47c06d84f5952f61b6549bf5f98c582506a54
altroz <nhb913@outlook.com>
2015-06-17 16:37:34 -0700

    Using the Null Reference to make Objects explicitly eligible for collections.

---
+++ src/GarbageCollectionTest/TestGarbageCollection.java
@@ -0,0 +1,10 @@
+package GarbageCollectionTest;
+
+public class TestGarbageCollection {
+	public static void main(String[]args){
+		StringBuffer sb = new StringBuffer("HELLO");
+		System.out.println(sb);
+		
+		sb= null;
+	}
+}
