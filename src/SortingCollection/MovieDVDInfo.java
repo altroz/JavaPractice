050a491f2fa0ad1f5ac7e01b4bd873b5a3e0936c
altroz <nhb913@outlook.com>
2015-06-17 14:34:23 -0700

    Comparable Information Implemented. // Code has alot of errors.

--- src/SortingCollection/MovieDVDInfo.java
+++ src/SortingCollection/MovieDVDInfo.java
@@ -2,7 +2,8 @@ package SortingCollection;
 
 import java.util.*;
 
-public class MovieDVDInfo {
+
+public class MovieDVDInfo implements Comparable {
 	String movieName;
 	String movieType;
 	String Hero;
@@ -20,4 +21,15 @@ public class MovieDVDInfo {
 	ArrayList dvdList = new ArrayList();
 	System.out.println(dvdList);
 	Collections.sort(dvdList);
+	
+	public int compareTo(MovieDVDInfo d){
+		return movieName.compareTo(d.getTitle());
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		MoiveDVDInfo d = (MoiveDVDInfo)o;
+		return movieName.compareTo(d.getTitle());
+	}
 }
